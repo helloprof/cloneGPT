11cbff6912756513532bfd5ba18f33883058fb35
helloprof <suluxan.seneca@gmail.com>
2024-06-20 17:58:33 -0400

    chats and topics data

---
+++ js/chats.js
@@ -0,0 +1,30 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.chats = [
+    {
+      chatID: "CID-1",
+      topicID: "TID-1",
+      details: "A protein is a large, complex molecule made up of amino acids and is essential for the structure, function, and regulation of the body's tissues and organs. Proteins are involved in virtually every process within cells and perform a vast array of functions.",
+      date: "2024-06-20",
+      verified: true
+    },
+    {
+        chatID: "CID-2",
+        topicID: "TID-2",
+        details: "A neural network is a computational model inspired by the way biological neural networks in the human brain process information. These networks are used in machine learning and artificial intelligence to recognize patterns, classify data, and make predictions. ",
+        date: "2024-05-20",
+        verified: true
+    },
+    {
+        chatID: "CID-3",
+        topicID: "TID-3",
+        details: "Explore Tokyo's vibrant neighborhoods and visit iconic landmarks like the Shibuya Crossing and Senso-ji Temple. Take a day trip to Hakone for stunning views of Mount Fuji and relaxing hot springs. Conclude your journey in Kyoto, marveling at historic temples such as Fushimi Inari Shrine and Kinkaku-ji (the Golden Pavilion).",
+        date: "2024-05-20",
+        verified: true
+    },
+  ];
+  
---
+++ js/topics.js
@@ -0,0 +1,27 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.topics = [
+    {
+        topicID: "TID-1",
+        name: "Biology",
+        description: "What's a protein?",
+        icon: "fa-solid fa-dna"
+    },
+    {
+        topicID: "TID-2",
+        name: "Machine Learning",
+        description: "What is a neural network?",
+        icon: "fa-solid fa-brain"
+    },
+    {
+        topicID: "TID-3",
+        name: "Travel",
+        description: "Make an itinerary for Japan in May",
+        icon: "fa-solid fa-plane"
+    },
+
+];
